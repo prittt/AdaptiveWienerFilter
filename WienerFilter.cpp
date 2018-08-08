ad5174f2fded902a10d4471c92828301eea680d2
Federico Bolelli <lk_number1@hotmail.it>
2018-08-08 18:34:19 +0200

    Update license

--- WienerFilter.cpp
+++ WienerFilter.cpp
@@ -1,4 +1,4 @@
-/*Copyright(c) 2017 Federico Bolelli
+/*Copyright(c) 2017-2018 Federico Bolelli, Costantino Grana
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -11,7 +11,7 @@ list of conditions and the following disclaimer.
 this list of conditions and the following disclaimer in the documentation
 and / or other materials provided with the distribution.
 
-* Neither the name of YACCLAB nor the names of its
+* Neither the name of AdaptiveWienerFilter nor the names of its
 contributors may be used to endorse or promote products derived from
 this software without specific prior written permission.
 
